8a620a8ccdf6b49d2aacab295084096287c03a45
InTak Han <hits0304@gmail.com>
2025-05-19 16:40:03 +0900

    [CHORE] #9 목표 설정 부분 레이아웃 수정

--- Dietto/Dietto/Presentation/Onboarding/View/TutorialView.swift
+++ Dietto/Dietto/Presentation/Onboarding/View/TutorialView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct TutorialView: View {
     @State var weigt: String = ""
     @State var distance: String = ""
+    
     @State var selectWeight = 60
     
     let items: [Int] = Array(20...100).reversed()
@@ -26,30 +27,32 @@ struct TutorialView: View {
                     .foregroundColor(.accent)
                 Text("알려주세요")
             }
+            
         }
         .font(.pretendardBold28)
         .padding()
         
         
+        
         VStack { //목표 몸무게 입력 부분
             HStack{
-                TextField("목표 체중", text: $weigt)
-                    .textFieldStyle(.roundedBorder)
-                    .border(Color.accentColor)
-//                Picker(selection: $selectWeight) {
-//                    ForEach(items, id: \.self) { item in
-//                        Text("\(item)")
-//                    }
-//                } label: {
-//                    Text("weight")
-//                }
-//                .pickerStyle(.wheel)
+                TextField("목표체중", text: $weigt)
+                    .keyboardType(.numberPad)
+                    .multilineTextAlignment(.center)
+                    .font(.pretendardMedium20)
+                    .foregroundColor(.primary)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 15)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color(.accent), lineWidth: 2)
+                    )
                 
                 Text("kg")
                     .foregroundColor(.accent)
                     .font(.AppLogo)
             }
-            .frame(width: 150)
+            .frame(width: 200)
             
         }
         .padding()
@@ -57,15 +60,23 @@ struct TutorialView: View {
         VStack{
             //목표 거리 입력하는 부분
             HStack{
-                TextField("목표 거리", text: $distance)
-                    .textFieldStyle(.roundedBorder)
-                    .border(Color.accentColor)
+                TextField("거리", text: $distance)
+                    .keyboardType(.numberPad)
+                    .multilineTextAlignment(.center)
+                    .font(.pretendardMedium20)
+                    .foregroundColor(.primary)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 15)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color(.accent), lineWidth: 2)
+                    )
                 
                 Text("km")
                     .foregroundColor(.accent)
                     .font(.AppLogo)
             }
-            .frame(width: 150)
+            .frame(width: 200)
             
         }
         .padding()
@@ -74,7 +85,7 @@ struct TutorialView: View {
             
         }.offset(y:180)
         
-        VStack{ //하단 버튼 부분
+        VStack{ //하단 버튼 부분 - 관심사 클릭 부분
             Button("다음") {
                 
             }
