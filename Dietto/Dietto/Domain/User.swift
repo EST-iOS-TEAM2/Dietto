46e82d3f22d166879b7139855083047533b8debd
JungHm <taggma12@gmail.com>
2025-05-20 09:18:08 +0900

    [FEAT] #14 유저 데이터 모델 추가

---
+++ Dietto/Dietto/Domain/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  Dietto
+//
+//  Created by 안정흠 on 5/19/25.
+//
+
+import Foundation
+
+struct User {
+    var name: String
+    var birth: Date
+    var gender: String
+    var height: Int
+    var weight: Int
+    var targetWeight: Int
+    var targetDistance: Int
+    var favorite: [String]
+}
