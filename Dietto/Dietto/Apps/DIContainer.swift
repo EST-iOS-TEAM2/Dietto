d68cc53ca35aa4f3f295a25638b1ed5be2cf0943
JungHm <taggma12@gmail.com>
2025-06-09 14:02:42 +0900

    Fix TestData Injection place

--- Dietto/Dietto/Apps/DIContainer.swift
+++ Dietto/Dietto/Apps/DIContainer.swift
@@ -25,6 +25,25 @@ final class DIContainer {
         self.weightHistoryUsecase = WeightHistoryUsecaseImpl(repository: AnotherStorageRepositoryImpl<WeightDTO>())
         self.ingredientUsecase = IngredientUsecaseImpl(repository: AnotherStorageRepositoryImpl<IngredientDTO>())
         
+        Task {
+            let temp = [
+                WeightEntity(date: Date()-(86400*8), scale: 20),
+                WeightEntity(date: Date()-(86400*7), scale: 30),
+                WeightEntity(date: Date()-(86400*6), scale: 40),
+                WeightEntity(date: Date()-(86400*5), scale: 55),
+                WeightEntity(date: Date()-(86400*4), scale: 63),
+                WeightEntity(date: Date()-(86400*3), scale: 70),
+                WeightEntity(date: Date()-(86400*2), scale: 65),
+                WeightEntity(date: Date()-86400, scale: 55),
+//                WeightEntity(date: Date(), scale: 50)
+            ]
+            
+            for i in temp {
+                try await self.weightHistoryUsecase.addNewWeight(weight: i.scale, date: i.date)
+            }
+        }
+        
+        
         //        Task.detached(priority: .background) { [weak self] in
         //            self?.interestsUsecase = InterestsUsecaseImpl(repository: AnotherStorageRepositoryImpl<InterestsDTO>())
         //            self?.userStorageUsecase = UserStorageUsecaseImpl(storage: AnotherStorageRepositoryImpl<UserDTO>())
